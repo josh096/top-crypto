a41394daf2527fd39996802e4af16e444d3a5e96
josh096 <joshw096@gmail.com>
2019-09-04 15:42:22 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api.coincap.io/v2/assets?limit=20').then(res => {
+            setCoins(res.data.data);
+            console.log(res.data.data);
+        });
+    }, []);
+
+    const LoadingComp = () => {
+        return <h1>Loading</h1>;
+    };
+
+    const CoinRows = ({coins}) => {
+        return (
+            <>
+                {coins.map(coin => {
+                    const floatPrice = parseFloat(coin.priceUsd);
+                    const price = floatPrice.toPrecision(5);
+                    return (
+                        <tr>
+                            <td>{coin.rank}</td>
+                            <td>{coin.name}</td>
+                            <td>${price}</td>
+                            <td>
+                                {Math.floor(coin.marketCapUsd).toLocaleString()}
+                            </td>
+                        </tr>
+                    );
+                })}
+            </>
+        );
+    };
+
+    const CoinTable = ({coins}) => {
+        return (
+            <>
+                <table class='table'>
+                    <thead>
+                        <tr>
+                            <th>
+                                <p title='Rank'>Rank</p>
+                            </th>
+                            <th>
+                                <p title='Name'>Name</p>
+                            </th>
+                            <th>
+                                <p title='Price'>Price</p>
+                            </th>
+                            <th>
+                                <p title='Market Cap'>Market Cap</p>
+                            </th>
+                            <th>
+                                <p title='Price'>Change(24hr)</p>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <CoinRows coins={coins} />
+                    </tbody>
+                </table>
+            </>
+        );
+    };
+
+    return <>{coins.length ? <CoinTable coins={coins} /> : <LoadingComp />}</>;
 }
 
 export default App;
