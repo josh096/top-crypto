1a29fba98444031710210fc95b21fc9ed000e859
josh096 <joshw096@gmail.com>
2019-09-05 09:48:05 +0100

    Added 5s updates for data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Router, Route, Link, Switch } from "react-router-dom";
-import "./App.css";
-import axios from "axios";
+import React, {useState, useEffect} from 'react';
+import {Router, Route, Link, Switch} from 'react-router-dom';
+import './App.css';
+import axios from 'axios';
 
 function App() {
     const [coins, setCoins] = useState([]);
 
     useEffect(() => {
-        axios.get("https://api.coincap.io/v2/assets?limit=20").then(res => {
-            setCoins(res.data.data);
-            console.log(res.data.data);
-        });
+        const getData = () => {
+            axios.get('https://api.coincap.io/v2/assets?limit=20').then(res => {
+                setCoins(res.data.data);
+                console.log(res.data.data);
+            });
+        };
+        getData();
+        setInterval(() => {
+            getData();
+        }, 5000);
     }, []);
 
     const LoadingComp = () => {
         return <h1>Loading</h1>;
     };
 
-    const CoinRows = ({ coins }) => {
+    const CoinRows = ({coins}) => {
         return (
             <>
                 {coins.map(coin => {
@@ -41,27 +47,27 @@ function App() {
         );
     };
 
-    const CoinTable = ({ coins }) => {
+    const CoinTable = ({coins}) => {
         return (
-            <div class="columns">
-                <div class="column is-half is-offset-one-quarter">
-                    <table class="table is-fullwidth is-hoverable">
+            <div class='columns'>
+                <div class='column is-half is-offset-one-quarter'>
+                    <table class='table is-fullwidth is-hoverable'>
                         <thead>
                             <tr>
                                 <th>
-                                    <p title="Rank">Rank</p>
+                                    <p title='Rank'>Rank</p>
                                 </th>
                                 <th>
-                                    <p title="Name">Name</p>
+                                    <p title='Name'>Name</p>
                                 </th>
                                 <th>
-                                    <p title="Price">Price</p>
+                                    <p title='Price'>Price</p>
                                 </th>
                                 <th>
-                                    <p title="Market Cap">Market Cap</p>
+                                    <p title='Market Cap'>Market Cap</p>
                                 </th>
                                 <th>
-                                    <p title="Price">Change(24hr)</p>
+                                    <p title='Price'>Change(24hr)</p>
                                 </th>
                             </tr>
                         </thead>
